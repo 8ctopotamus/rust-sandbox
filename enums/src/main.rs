9e553b93c6ae58c9780c03f7d1a0d9af84ae787b
8ctopotamus <joshnaylor88@gmail.com>
2022-12-17 21:59:01 -0600

    pattern matching

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,18 +1,30 @@
 #[derive(Debug)]
-enum Message {
-    Quit,
-    Move { x: i32, y: i32 },
-    Write(String),
-    ChangeColor(i32, i32, i32),
+enum UsState {
+    Alabama,
+    Alaska,
+    // ...
 }
 
-impl Message {
-    fn call(&self) {
-        dbg!(self);
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        },
     }
 }
 
 fn main() {
-    let m = Message::Write(String::from("hello"));
-    m.call();
+    let coin: u8 = value_in_cents(Coin::Quarter(UsState::Alaska));
+    println!("{coin}c");
 }
