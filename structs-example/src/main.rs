d180f5d1a3ab78e68da3bcc50dec6c1b9db902ab
Josh <zylo.codes@gmail.com>
2022-12-17 19:05:36 -0600

    associated functions

--- structs-example/src/main.rs
+++ structs-example/src/main.rs
@@ -5,6 +5,15 @@ struct Rectangle {
 }
 
 impl Rectangle {
+    // associated function
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+
+    // methods (have self as a param)
     fn area(&self) -> u32 {
         &self.width * &self.height
     }
@@ -30,6 +39,9 @@ fn main() {
 
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let square = Rectangle::square(100);
+    dbg!(square);
 }
 
 
