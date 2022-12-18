e79e8fc3cdf89c9770021ea454338f6955685602
8ctopotamus <joshnaylor88@gmail.com>
2022-12-17 20:45:04 -0600

    enums

---
+++ enums/src/main.rs
@@ -0,0 +1,33 @@
+#[derive(Debug)]
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+#[derive(Debug)]
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+// fn route(_ip_type: IpAddrType) {
+    
+// }
+
+fn main() {
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+    
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+
+    dbg!(home);
+    dbg!(loopback);
+
+    // route(home);
+    // route(loopback);
+}
