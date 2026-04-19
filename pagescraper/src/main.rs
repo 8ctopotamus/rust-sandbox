ff7c6a4a167113c648405693c57148356e18bbda
Josh <zylo.codes@gmail.com>
2026-04-19 11:27:41 -0500

    simple web scraper

---
+++ pagescraper/src/main.rs
@@ -0,0 +1,45 @@
+use std::fs::OpenOptions;
+use std::io::{Write};
+use reqwest;
+use scraper::{Html, Selector};
+
+let url = "https://www.country-codes.org/us-area-codes";
+let output_path = "area-codes.csv";
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let html = reqwest::get(url)
+        .await?
+        .text()
+        .await?;
+    
+    // println!("{html:#?}");
+
+    let document = Html::parse_document(&html);
+    let area_codes_selector = Selector::parse(".area-code-badge strong").unwrap();
+    let area_codes =  document.select(&area_codes_selector);
+
+    let mut file = OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .create(true)
+        .open()
+        .expect("Unable to open file");
+
+    for element in area_codes {
+        let mut area_code = element
+            .text()
+            .collect::<String>();
+        
+        // println!("{:#?}", area_code);
+
+        area_code.push_str("\n");
+        
+        // file.write_all(area_code.as_bytes()).expect("Unable to append data");
+        writeln!(file, "{}", area_code);
+    }
+
+    println("")
+
+    Ok(())
+}
